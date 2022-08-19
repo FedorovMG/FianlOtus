5556a003a10392979d064515467b702ec7093f70
FedorovMG <fedorovmg@hotmail.com>
2022-08-19 22:34:53 +0500

    Add varibels

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
-    yandex={
-        source = "yandex-cloud/yandex"
+    yandex = {
+      source = "yandex-cloud/yandex"
     }
   }
 }
 
 provider "yandex" {
-  token=""
-  cloud_id=""
-  folder_id=""
-  zone=""
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
 }
---
+++ variebles.tf
@@ -0,0 +1,19 @@
+variable "token" {
+    type = string
+    description = "Token YandexCloud"
+}
+
+variable "cloud_id" {
+    type = string
+    description = "ID cloud"
+}
+
+variable "folder_id" {
+    type = string
+    description = "ID folder"
+}
+
+variable "zone" {
+    type = string
+    description = "Zone"
+}
