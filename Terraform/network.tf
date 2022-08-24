2fe92e2d1bec1e60fc31df707fb87726e7afcc70
FedorovMG <FedorovMG@hotmail.com>
2022-08-24 10:37:10 +0500

    Add config clickhouse

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -8,7 +8,7 @@ resource "yandex_vpc_subnet" "data_subnet" {
   network_id = yandex_vpc_network.data_net.id
   v4_cidr_blocks = [ "10.0.1.0/24" ]
 }
-
+/*
 resource "yandex_vpc_security_group" "sg" {
   network_id = yandex_vpc_network.data_net.id
   ingress {
@@ -18,3 +18,4 @@ resource "yandex_vpc_security_group" "sg" {
     to_port = 65535
   }
 }
+*/
